b1e55b560be755cedaa4eae4576ce05c96c7da99
a <arbiterqq@gmail.com>
2021-01-12 15:14:25 +0800

    chapter 1 completed

---
+++ part1/ch1/3.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |3|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define str "helloworld")
+(define i 5)
+(define pre (substring str 0 i))
+(define suf (substring str i))
+(string-append pre " " suf)
