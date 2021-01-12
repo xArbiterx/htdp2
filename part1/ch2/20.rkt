63000e1bda463202b7b8450ba35fefed33c6b016
a <arbiterqq@gmail.com>
2021-01-12 17:20:18 +0800

    chapter 2 work finished

---
+++ part1/ch2/20.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |20|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (string-delete str i)
+  (if (> (string-length str) 1)
+      (if (= (string-length str) i)
+          (substring str 0 (- i 1))
+          (if (> (string-length str) i)
+              (string-append(substring str 0 (- i 1)) (substring str i))
+              "err"))
+      ""))
+(string-delete "str" 4)
